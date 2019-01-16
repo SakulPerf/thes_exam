db25987bf837e81259d306a0504da95b78660c36
Sakul Jaruthanaset <miolynet@perfenterprise.com>
2019-01-16 13:59:24 +0700

    Complete manage product page.

--- ex02/shopmobile/src/pages/home/home.ts
+++ ex02/shopmobile/src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { HttpClient } from '@angular/common/http';
+import { ProductInfo } from '../../models/productInfo';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  private name: string;
+  private price: number;
+  private products: ProductInfo[] = [];
 
+  constructor(public navCtrl: NavController, public http: HttpClient) {
+  }
+
+  ionViewDidLoad() {
+    this.getAllProducts();
+  }
+
+  AddNewProduct() {
+    this.http.post<ProductInfo[]>("https://localhost:44373/api/shop",
+      {
+        price: this.price,
+        name: this.name
+      }).subscribe(
+        it => {
+          this.products = it;
+        },
+        error => {
+          console.log(error);
+        });
+  }
+
+  private getAllProducts(){
+    this.http.get<ProductInfo[]>("https://localhost:44373/api/shop").subscribe(
+      it => {
+        this.products = it;
+      },
+      error => {
+        console.log(error);
+      });
   }
 
 }
